a855df26d94d72a1cdd3649f35d21f4d2290fb59
Mickaël Derriey <mderriey@gmail.com>
2017-05-12 16:47:39 +1000

    Implement first API methods

---
+++ RediGrowth/RediGrowth.Api/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RediGrowth.Api.Models;
+using RediGrowth.Domain;
+
+namespace RediGrowth.Api.Controllers
+{
+    [Route("api/diaryentries")]
+    public class DiaryEntriesController: Controller
+    {
+        private static List<DiaryEntry> _entries = new List<DiaryEntry>()
+        {
+            new DiaryEntry(Guid.NewGuid(), DateTime.Today, Guid.NewGuid(), 3),
+            new DiaryEntry(Guid.NewGuid(), DateTime.Today, Guid.NewGuid(), 4),
+            new DiaryEntry(Guid.NewGuid(), DateTime.Today, Guid.NewGuid(), 2)
+        };
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<DiaryEntry> GetAll()
+        {
+            return _entries; 
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public ActionResult Get(Guid id)
+        {
+            var entry = _entries.FirstOrDefault(e => e.Id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return Ok(entry);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult Add([FromBody]AddDiaryEntryViewModel entry)
+        {
+            if (_entries.Any(n => n.Id == entry.Id))
+            {
+                return BadRequest($"An entry with {entry.Id} id already exists");
+            }
+            var e = new DiaryEntry(entry.Id, entry.Date, entry.PersonId, entry.Rating);
+            entry.Notes.ForEach(n => e.AddNote(n.Id, n.Text));
+            _entries.Add(e);
+            return Created(Url.Action(nameof(this.Get), new { id = e.Id }), null);
+        }
+
+    }
+}
---
+++ RediGrowth/RediGrowth.Api/Models/AddDiaryEntryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RediGrowth.Api.Models
+{
+    public class AddDiaryEntryViewModel
+    {
+        public class DiaryNote
+        {
+            public Guid Id { get; set; }
+            public string Text { get; set; }
+        }
+
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public Guid PersonId { get; set; }
+        public int Rating { get; set; }
+        public List<DiaryNote> Notes { get; set; }
+    }
+}
